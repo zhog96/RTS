3def7308107086ff8dc49ba43d6ae3144f135dc7
Жогов Александр <zogovaleksandr@MacBook-Pro-Zogov.local>
2019-03-01 18:25:54 +0300

    Test dragon

--- SFML/OS X(mac)/SFML/SFML/main.cpp
+++ SFML/OS X(mac)/SFML/SFML/main.cpp
@@ -1,75 +1,139 @@
+#include <stdio.h>
+
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
-// Here is a small helper for you! Have a look.
 #include "ResourcePath.hpp"
 
-int main(int, char const**)
-{
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+// This class is not for using, fun only
+class SomeAnimation {
+    sf::Sprite& sprite;
+    sf::Vector2<int> tileSize;
+    int cadr = 0;
+    int line = 0;
+    int micros;
+    int cadrCount;
+    int microsPerCadr;
+    bool freeze = true;
+public:
+    SomeAnimation(sf::Texture& texture, sf::Vector2<float> position, sf::IntRect texture_rect, int microsPerCadr, int cadrCount) : sprite(* (new sf::Sprite(texture))) {
+        sprite.setPosition(position);
+        sprite.setTextureRect(texture_rect);
+        sprite.setScale(3.0, 3.0);
+        tileSize = sf::Vector2<int>(texture_rect.width, texture_rect.height);
+        this->cadrCount = cadrCount;
+        this->microsPerCadr = microsPerCadr;
+    }
+    
+    void move(sf::Vector2<float> dr) {
+        sprite.setPosition(sprite.getPosition() + dr);
+    }
+    
+    void changeLine(int line) {
+        this->line = line;
+    }
+    
+    void setFreeze(bool freeze) {
+        this->freeze = freeze;
+    }
+    
+    void update(sf::Time dt) {
+        if(!freeze) {
+            micros += dt.asMicroseconds();
+            micros %= cadrCount * microsPerCadr;
+            cadr = micros / microsPerCadr;
+        }
+    }
+    
+    void draw(sf::RenderWindow& window) {
+        sprite.setTextureRect(sf::IntRect(tileSize.x * cadr, tileSize.y * line, tileSize.x, tileSize.y));
+        window.draw(sprite);
+    }
+};
 
-    // Set the Icon
+// This class is not for using, fun only
+class SomeThing {
+    SomeAnimation& animation;
+    float speed;
+public:
+    SomeThing(SomeAnimation& animation) : animation(* (new SomeAnimation(animation))) {
+        speed = 350.0;
+    }
+    
+    void update(sf::Time dt) {
+        float dx = 0.0, dy = 0.0;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            dy -= dt.asMicroseconds() * (speed / 1000000);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            dy += dt.asMicroseconds() * (speed / 1000000);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            dx -= dt.asMicroseconds() * (speed / 1000000);
+            dy = 0;
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            dx += dt.asMicroseconds() * (speed / 1000000);
+            dy = 0;
+        }
+        
+        if(dy > 0) animation.changeLine(0);
+        if(dx < 0) animation.changeLine(1);
+        if(dx > 0) animation.changeLine(2);
+        if(dy < 0) animation.changeLine(3);
+        if(dx * dx + dy * dy == 0) animation.setFreeze(true);
+        else animation.setFreeze(false);
+        
+        animation.move(sf::Vector2<float>(dx, dy));
+        animation.update(dt);
+    }
+    
+    void draw(sf::RenderWindow& window) {
+        animation.draw(window);
+    }
+};
+
+int main(int, char const**) {
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML window");
+
     sf::Image icon;
     if (!icon.loadFromFile(resourcePath() + "icon.png")) {
         return EXIT_FAILURE;
     }
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
-    // Load a sprite to display
     sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
+    if (!texture.loadFromFile(resourcePath() + "image.axd.png")) {
         return EXIT_FAILURE;
     }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
-    // Start the game loop
+    
+    SomeAnimation animation(texture, sf::Vector2<float>(100.0, 100.0), sf::IntRect(0, 0, 107, 106), 150000, 3);
+    SomeThing something(animation);
+    
+    sf::Clock deltaClock;
+    
     while (window.isOpen())
     {
-        // Process events
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // Close window: exit
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-
-            // Escape pressed: exit
+            
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
                 window.close();
             }
         }
-
-        // Clear screen
+        
+        sf::Time dt = deltaClock.restart();
+        
+        something.update(dt);
+        
         window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
+        
+        something.draw(window);
         window.display();
     }
-
     return EXIT_SUCCESS;
 }
