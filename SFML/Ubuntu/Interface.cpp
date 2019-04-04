782758e63f1e5e30481f386f3da6c9ef7ddd58c7
sysak_ma <sysak.ma@phystech.edu>
2019-04-04 17:41:01 +0300

    Added mouse compatibility for menu (raw)

--- SFML/Ubuntu/Interface.cpp
+++ SFML/Ubuntu/Interface.cpp
@@ -16,10 +16,10 @@ void Interface::MainMenu(int button, sf::Texture & textureBG, sf::Texture & text
     window.draw(spriteBG);
 
     //Title
-    sf::Text title("Hello RTS", Interface::font, 120);
-    title.setFillColor(sf::Color::Green);
+    sf::Text title("RTS Main Menu", Interface::font, 150);
+    title.setFillColor(sf::Color::Cyan);
     sf::Transform titleTranslate;
-    titleTranslate.translate(700, 50);
+    titleTranslate.translate(400, 50);
     sf::RenderStates titleRS(titleTranslate);
     window.draw(title, titleRS);
 
@@ -32,13 +32,14 @@ void Interface::MainMenu(int button, sf::Texture & textureBG, sf::Texture & text
     window.draw(spriteB1, B1RS);
     window.draw(spriteB2, B2RS);
     sf::Text textB1("Play", Interface::font, 60), textB2("Exit", Interface::font, 60);
+    textB1.setFillColor(button?sf::Color::White:sf::Color::Yellow);
+    textB2.setFillColor(!button?sf::Color::White:sf::Color::Yellow);
     sf::Transform trTextB1, trTextB2;
     trTextB1.translate(670, 450);
     trTextB2.translate(670, 750);
     sf::RenderStates textB1RS(trTextB1), textB2RS(trTextB2);
     window.draw(textB1, textB1RS);
     window.draw(textB2, textB2RS);
-
 }
 
 int Interface::run() {
@@ -46,38 +47,43 @@ int Interface::run() {
     // state of the system : MainMenu, Game, ...
     enum {MainMenu, something_else};
     int state = MainMenu;
+
     // Main Menu variables
     int MainMenuButton = 0;
     sf::Texture MainMenuTextureBG, MainMenuTextureB1, MainMenuTextureB2;
-    MainMenuTextureBG.loadFromFile("textures/MainMenuBG.jpg");
-    MainMenuTextureB1.loadFromFile("textures/buttonPressed.png");
-    MainMenuTextureB2.loadFromFile("textures/buttonNotPressed.png");
+    MainMenuTextureBG.loadFromFile("textures/MainMenu/MainMenuBG.jpg");
+    MainMenuTextureB1.loadFromFile("textures/MainMenu/buttonPressed.png");
+    MainMenuTextureB2.loadFromFile("textures/MainMenu/buttonNotPressed.png");
 
     while(window.isOpen()) {
         sf::Event event;
+        sf::Mouse mouse;
         while(window.pollEvent(event)) {
-            if(event.type == sf::Event::Closed) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
-            }
-            if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
-            if(event.type == sf::Event::KeyPressed) {
+            } else if (event.type == sf::Event::KeyPressed) {
                 q_Core_out.push(event.key.code);
+                if (event.key.code == sf::Keyboard::Escape) {
+                    window.close();
+                } else if (state == MainMenu && (event.key.code == sf::Keyboard::Up || event.key.code == sf::Keyboard::Down)) {
+                    MainMenuButton = !MainMenuButton;
+                } else if (state == MainMenu && MainMenuButton == 1 && event.key.code == sf::Keyboard::Return) {
+                    window.close();
+                } else if (state == MainMenu && MainMenuButton == 0 && event.key.code == sf::Keyboard::Return) {
+                    state = something_else;
+                } else if (state == something_else && event.key.code == sf::Keyboard::Space) {
+                    state = MainMenu;
+                }
+            } else if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    if (mouse.getPosition(window).x < 1230 && mouse.getPosition(window).x > 630 && mouse.getPosition(window).y > 400 && mouse.getPosition(window).y < 600) {
+                        state = something_else;
+                    } else if (mouse.getPosition(window).x < 1230 && mouse.getPosition(window).x > 630 && mouse.getPosition(window).y > 700 && mouse.getPosition(window).y < 900) {
+                        window.close();
+                    }
+                }
             }
-            if(event.type == sf::Event::KeyPressed && (event.key.code == sf::Keyboard::Up || event.key.code == sf::Keyboard::Down)) {
-                MainMenuButton++;
-                MainMenuButton %= 2;
-            }
-            if(state == MainMenu && event.type == sf::Event::KeyPressed && MainMenuButton == 1 && event.key.code == sf::Keyboard::Return) {
-                window.close();
-            }
-            if(state == MainMenu && event.type == sf::Event::KeyPressed && MainMenuButton == 0 && event.key.code == sf::Keyboard::Return) {
-                state = something_else;
-            }
-            if(state == something_else && event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space) {
-                state = MainMenu;
-            }
+
         }
 
         if(!q_Core_in.isEmpty()) {
@@ -87,6 +93,11 @@ int Interface::run() {
         }
 
         if (state == MainMenu) {
+            if (mouse.getPosition(window).x < 1230 && mouse.getPosition(window).x > 630 && mouse.getPosition(window).y > 400 && mouse.getPosition(window).y < 600) {
+                MainMenuButton = 0;
+            } else if (mouse.getPosition(window).x < 1230 && mouse.getPosition(window).x > 630 && mouse.getPosition(window).y > 700 && mouse.getPosition(window).y < 900) {
+                MainMenuButton = 1;
+            }
             Interface::MainMenu(MainMenuButton, MainMenuTextureBG, MainMenuTextureB1, MainMenuTextureB2);
         } else if (state == something_else) {
             sf::Text title("Work in progress (kind of)\n Press ESC to close\n the window\n or SPACE to go back to\n main menu", Interface::font, 120);
