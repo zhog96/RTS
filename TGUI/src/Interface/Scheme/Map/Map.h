6029c51ec21209da6bf81335b29b8842ab105d72
Alex <zhogov.aa@phystech.edu>
2019-05-22 23:15:01 +0300

    Threads

--- TGUI/src/Interface/Scheme/Map/Map.h
+++ TGUI/src/Interface/Scheme/Map/Map.h
@@ -13,15 +13,20 @@ public:
     static tgui::Canvas * canvas;
     static sf::Vector2f camera;
     static float height;
+    static bool openTiles;
     static long long timeBoom;
+    static sf::Vector2f openZeros;
     static std::vector<sf::Vector2f> positionBoom;
 
     static void loadMap(tgui::Canvas * canvas);
     static void update();
     static void clean();
 
-    static void checkTileStates();
-    static void openAllTiles();
+    static void switchCase(sf::Vector2i task);
+    static void updateObjects(sf::Vector2i task);
+
+    static void checkTileStates(sf::Vector2i task);
+    static void openAllTiles(sf::Vector2i task);
     static void win();
     static void lose();
 };
