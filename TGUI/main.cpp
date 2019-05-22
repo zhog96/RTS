85ef6fea0e32540dca92c16baaa6c0c2980587e7
Alex <zhogov.aa@phystech.edu>
2019-05-23 00:25:50 +0300

    Core cutted

--- TGUI/main.cpp
+++ TGUI/main.cpp
@@ -8,31 +8,10 @@
 #include <sys/types.h>
 
 #include "src/Interface/Interface.hpp"
-#include "src/Core/Core.hpp"
-#include "src/Tools/Queue.hpp"
-
-void interfaceDaemon() {
-    Interface::initInterface();
-    printf("interfaceDaemon start\n");
-    Interface::run();
-    printf("interfaceDaemon stop\n");
-}
-
-void coreDaemon() {
-    Core::initCore();
-    printf("coreDaemon start\n");
-    Core::run();
-    printf("coreDaemon stop\n");
-}
 
 int main() {
-    sf::Thread thread(&interfaceDaemon);
-    thread.launch();
-    sf::Thread thread2(&coreDaemon);
-    thread2.launch();
-    thread.wait();
-    Core::stop();
-    thread2.wait();
+    Interface::initInterface();
+    Interface::run();
     printf("main is done\n");
     //interfaceDaemon();
     return EXIT_SUCCESS;
--- TGUI/src/Interface/Interface.cpp
+++ TGUI/src/Interface/Interface.cpp
@@ -36,57 +36,56 @@ int Interface::run() {
     void (*update)() = nullptr;
 
     // Main loop
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         UIinformation::mPos = sf::Vector2f(sf::Mouse::getPosition(window));
 
-        for(int i = 0; i < 256; i++) {
-            if(!UIinformation::bPressed[i]) UIinformation::bClicked[i] = false;
+        for (int i = 0; i < 256; i++) {
+            if (!UIinformation::bPressed[i]) UIinformation::bClicked[i] = false;
         }
-        for(int i = 0; i < 2; i++) {
-            if(!UIinformation::mPressed[i]) UIinformation::mClicked[i] = false;
+        for (int i = 0; i < 2; i++) {
+            if (!UIinformation::mPressed[i]) UIinformation::mClicked[i] = false;
         }
-        for(int i = 0; i < 2; i++) {
-            if(!UIinformation::mPressed[i]) {
+        for (int i = 0; i < 2; i++) {
+            if (!UIinformation::mPressed[i]) {
                 UIinformation::mLastPressed[i] = UIinformation::mPos;
                 UIinformation::mLastClick[i] = UIinformation::mPos;
             }
         }
 
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
 
             // When the window is closed, the application ends
             if (event.type == sf::Event::Closed)
                 window.close();
                 // When the window is resized, the view is changed
             else if (event.type == sf::Event::Resized) {
-                window.setView(sf::View(sf::FloatRect(0.f, 0.f, static_cast<float>(event.size.width), static_cast<float>(event.size.height))));
+                window.setView(sf::View(sf::FloatRect(0.f, 0.f, static_cast<float>(event.size.width),
+                                                      static_cast<float>(event.size.height))));
                 gui.setView(window.getView());
             } else if (event.type == sf::Event::KeyPressed) {
                 //printf("Keyboard::Pressed: %d\n", event.key.code);
-                if(0 <= event.key.code < 256) {
+                if (0 <= event.key.code < 256) {
                     UIinformation::bPressed[event.key.code] = true;
                     UIinformation::bClicked[event.key.code] = false;
                 }
             } else if (event.type == sf::Event::KeyReleased) {
-                if(0 <= event.key.code < 256) {
-                    if(UIinformation::bPressed[event.key.code]) {
+                if (0 <= event.key.code < 256) {
+                    if (UIinformation::bPressed[event.key.code]) {
                         UIinformation::bClicked[event.key.code] = true;
                     }
                     UIinformation::bPressed[event.key.code] = false;
                 }
             } else if (event.type == sf::Event::MouseButtonPressed) {
                 //printf("Mouse::Pressed: %d\n", event.key.code);
-                if(0 <= event.key.code < 2) {
+                if (0 <= event.key.code < 2) {
                     UIinformation::mPressStart[event.key.code] = Time::time;
                     UIinformation::mPressed[event.key.code] = true;
                     UIinformation::mClicked[event.key.code] = false;
                 }
             } else if (event.type == sf::Event::MouseButtonReleased) {
-                if(0 <= event.key.code < 2) {
-                    if(UIinformation::mPressed[event.key.code]) {
+                if (0 <= event.key.code < 2) {
+                    if (UIinformation::mPressed[event.key.code]) {
                         UIinformation::mClicked[event.key.code] = true;
                     }
                     UIinformation::mPressed[event.key.code] = false;
@@ -99,13 +98,13 @@ int Interface::run() {
             gui.handleEvent(event);
         }
 
-        for(int i = 0; i < 2; i++) {
-            if(UIinformation::mClicked[i]) {
+        for (int i = 0; i < 2; i++) {
+            if (UIinformation::mClicked[i]) {
                 UIinformation::mDeltaClick[i] = UIinformation::mPos - UIinformation::mLastClick[i];
                 UIinformation::mLastClick[i] = UIinformation::mPos;
                 //printf("%f %f\n", UIinformation::mDeltaClick[0].x, UIinformation::mDeltaClick[0].y);
             }
-            if(UIinformation::mPressed[i]) {
+            if (UIinformation::mPressed[i]) {
                 UIinformation::mDeltaPressed[i] = UIinformation::mPos - UIinformation::mLastPressed[i];
                 UIinformation::mLastPressed[i] = UIinformation::mPos;
                 //printf("%f %f\n", UIinformation::mDeltaPressed[0].x, UIinformation::mDeltaPressed[0].y);
@@ -114,62 +113,52 @@ int Interface::run() {
             }
         }
 
-        for(int i = 0; i < 2; i++) {
-            if(UIinformation::mPressed[i]) UIinformation::mClickTime[i] = Time::time - UIinformation::mPressStart[i];
+        for (int i = 0; i < 2; i++) {
+            if (UIinformation::mPressed[i]) UIinformation::mClickTime[i] = Time::time - UIinformation::mPressStart[i];
         }
 
-        for(int i = 0; i < 2; i++) {
-            /*int delta = 6;
-            sf::Vector2f vec = UIinformation::mDeltaClick[i];
-            float len = vec.x * vec.x + vec.y * vec.y;
-            if(len <= delta && delta <= len) UIinformation::mDeltaClick[i] = sf::Vector2f(0.0f, 0.0f);*/
-            if(UIinformation::mClickTime[i] < 100000) {
+        for (int i = 0; i < 2; i++) {
+            if (UIinformation::mClickTime[i] < 100000) {
                 UIinformation::mDeltaClick[i] = sf::Vector2f(0.0f, 0.0f);
                 UIinformation::mDeltaPressed[i] = sf::Vector2f(0.0f, 0.0f);
             }
         }
 
         Time::update();
-        if(update != nullptr) update();
+        if (update != nullptr) update();
 
         switch (Shemes::shemeChange) {
             case ShemesEnum::None :
                 break;
             case ShemesEnum::Menu :
-                try
-                {
+                try {
                     Shemes::cleanShemes();
                     update = MenuSheme::update;
                     MenuSheme::loadSheme();
                 }
-                catch (const tgui::Exception& e)
-                {
+                catch (const tgui::Exception &e) {
                     std::cerr << "Failed to load TGUI widgets: " << e.what() << std::endl;
                     return 1;
                 }
                 break;
             case ShemesEnum::Play :
-                try
-                {
+                try {
                     Shemes::cleanShemes();
                     update = PlaySheme::update;
                     PlaySheme::loadSheme();
                 }
-                catch (const tgui::Exception& e)
-                {
+                catch (const tgui::Exception &e) {
                     std::cerr << "Failed to load TGUI widgets: " << e.what() << std::endl;
                     return 1;
                 }
                 break;
             case ShemesEnum::Lobby:
-                try
-                {
+                try {
                     Shemes::cleanShemes();
                     update = LobbySheme::update;
                     LobbySheme::loadSheme();
                 }
-                catch (const tgui::Exception& e)
-                {
+                catch (const tgui::Exception &e) {
                     std::cerr << "Failed to load TGUI widgets: " << e.what() << std::endl;
                     return 1;
                 }
